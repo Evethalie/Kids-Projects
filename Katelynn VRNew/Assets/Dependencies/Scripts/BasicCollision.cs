9d9218f39d178600fefc334ecfa25f6ea6bedd7a
Evethalie <148084606+Evethalie@users.noreply.github.com>
2025-04-03 16:41:25 -0700

    katlelynn

---
+++ Katelynn VRNew/Assets/Dependencies/Scripts/BasicCollision.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class BasicCollision : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("search");
+        }
+          
+          
+    }
+
+}
+
